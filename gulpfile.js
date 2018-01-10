15ca856a790932fb57f32e1f001367d87472655d
Martin Jurča <martin.jurca@firma.seznam.cz>
2018-01-10 09:42:08 +0100

    added implementation

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+'use strict'
+
+const del = require('del')
+const fs = require('fs')
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const rename = require('gulp-rename')
+const postCss = require('gulp-postcss')
+const postCustomProperties = require('postcss-custom-properties')
+const util = require('util')
+
+async function injectCss(done) {
+  const readFile = util.promisify(fs.readFile)
+  const writeFile = util.promisify(fs.writeFile)
+
+  const [css, es6] = await Promise.all([
+    readFile('./dist/szn-select-ui.css', 'utf-8'),
+    readFile('./dist/szn-select-ui.es6.js', 'utf-8'),
+  ])
+
+  await writeFile('./dist/szn-select-ui.es6.js', es6.replace('%{CSS_STYLES}%', css), 'utf-8')
+
+  done()
+}
+
+function compileJS() {
+  return gulp
+    .src('./dist/szn-select-ui.es6.js')
+    .pipe(babel({
+      presets: [['env', {
+        targets: {
+          browsers: ['ie 8'],
+        },
+      }]],
+    }))
+    .pipe(rename('szn-select-ui.es3.js'))
+    .pipe(gulp.dest('./dist'))
+}
+
+const copy = gulp.parallel(
+  copyES6Implementation,
+  copyPackageMetaFiles,
+)
+
+function copyPackageMetaFiles() {
+  return gulp
+    .src(['./LICENSE', './package.json', './README.md'])
+    .pipe(gulp.dest('./dist'))
+}
+
+function copyES6Implementation() {
+  return gulp
+    .src('./szn-select-ui.js')
+    .pipe(rename('szn-select-ui.es6.js'))
+    .pipe(gulp.dest('./dist'))
+}
+
+function compileCss() {
+  return gulp
+    .src('./szn-select-ui.css')
+    .pipe(postCss([
+      postCustomProperties({
+        preserve: true,
+      }),
+    ]))
+    .pipe(gulp.dest('./dist'))
+}
+
+function minify() {
+  return gulp
+    .src('./dist/*.js')
+    .pipe(babel({
+      presets: ['minify'],
+    }))
+    .pipe(rename({
+      suffix: '.min',
+    }))
+    .pipe(gulp.dest('./dist'))
+}
+
+function clean() {
+  return del('./dist')
+}
+
+function cleanup() {
+  return del('./dist/szn-select-ui.css')
+}
+
+exports.default = gulp.series(
+  clean,
+  gulp.parallel(
+    compileCss,
+    copy,
+  ),
+  injectCss,
+  compileJS,
+  minify,
+  cleanup,
+)
---
+++ szn-select-ui.js
@@ -0,0 +1,270 @@
+'use strict'
+;(global => {
+  const SznElements = global.SznElements = global.SznElements || {}
+
+  const CSS_STYLES = `
+%{CSS_STYLES}%
+  `
+  const CSS_STYLES_TAG = 'data-styles--szn-select'
+
+  const MIN_BOTTOM_SPACE = 160 // px
+  const OBSERVED_DOM_EVENTS = ['resize', 'scroll', 'wheel', 'touchmove']
+
+  let stylesInjected = false
+
+  SznElements['szn-select-ui'] = class SznSelectUi {
+    constructor(rootElement, uiContainer) {
+      if (!rootElement.hasOwnProperty('minBottomSpace')) {
+        Object.defineProperty(rootElement, 'minBottomSpace', {
+          get: () => rootElement._broker._minBottomSpace,
+          set: value => {
+            rootElement._broker._minBottomSpace = value
+            if (rootElement._broker._dropdown && rootElement._broker._dropdown._broker) {
+              rootElement._broker._dropdown.minBottomSpace = value
+            }
+          },
+        })
+      }
+
+      rootElement.setSelectElement = this.setSelectElement.bind(this)
+      rootElement.setFocus = this.setFocus.bind(this)
+      rootElement.setOpen = this.setOpen.bind(this)
+      rootElement.onUiInteracted = rootElement.onUiInteracted || null
+
+      this._root = rootElement
+      this._select = null
+      this._button = null
+      this._dropdown = null
+      this._dropdownPosition = null
+      this._dropdownContent = SznElements.buildDom('<szn- data-szn-select-dropdown data-szn-tethered-content></szn->')
+      this._dropdownOptions = null
+      this._dropdownContainer = document.body
+      this._blurTimeout = null
+      this._minBottomSpace = MIN_BOTTOM_SPACE
+      this._observer = new MutationObserver(onDomMutated.bind(this))
+
+      this._onDropdownPositionChange = verticalAlignment => onDropdownPositionChange(this, verticalAlignment)
+      this._onDropdownSizeUpdateNeeded = () => onDropdownSizeUpdateNeeded(this)
+
+      if (!stylesInjected) {
+        const stylesContainer = document.createElement('style')
+        stylesContainer.innerHTML = CSS_STYLES
+        stylesContainer.setAttribute(CSS_STYLES_TAG, '')
+        document.head.appendChild(stylesContainer)
+        stylesInjected = true
+      }
+    }
+
+    onMount() {
+      addEventListeners(this)
+      this._observer.observe(this._root, {
+        childList: true,
+        attributes: true,
+        characterData: true,
+        subtree: true,
+        attributeFilter: ['disabled', 'multiple', 'selected'],
+      })
+    }
+
+    onUnmount() {
+      if (this._dropdown) {
+        this._dropdown.parentNode.removeChild(this._dropdown)
+      }
+      if (this._blurTimeout) {
+        clearTimeout(this._blurTimeout)
+        this._blurTimeout = null
+      }
+
+      removeEventListeners(this)
+      this._observer.disconnect()
+    }
+
+    setSelectElement(select) {
+      this._select = select
+
+      createUI(this)
+    }
+
+    setOpen(isOpen) {
+      if (this._select.multiple || this._select.disabled) {
+        return
+      }
+
+      if (isOpen) {
+        if (this._button._broker) {
+          this._button.setOpen(true)
+        }
+        this._dropdownContainer.appendChild(this._dropdown)
+
+        let dropdownReady = false
+        let optionsReady = false
+        SznElements.awaitElementReady(this._dropdown, () => {
+          dropdownReady = true
+          if (optionsReady) {
+            initDropdown(this, this._dropdown, this._dropdownOptions)
+          }
+        })
+        SznElements.awaitElementReady(this._dropdownOptions, () => {
+          optionsReady = true
+          if (dropdownReady) {
+            initDropdown(this, this._dropdown, this._dropdownOptions)
+          }
+        })
+      } else {
+        if (!this._dropdown.parentNode) {
+          return
+        }
+
+        if (this._button._broker) {
+          this._button.setOpen(false)
+        }
+        this._dropdown.parentNode.removeChild(this._dropdown)
+      }
+    }
+
+    setFocus(hasFocus) {
+      if (hasFocus) {
+        this._root.setAttribute('data-szn-select-active', '')
+      } else {
+        this._root.removeAttribute('data-szn-select-active')
+      }
+    }
+  }
+
+  function addEventListeners(instance) {
+    for (const eventType of OBSERVED_DOM_EVENTS) {
+      addEventListener(eventType, instance._onDropdownSizeUpdateNeeded)
+    }
+  }
+
+  function removeEventListeners(instance) {
+    for (const eventType of OBSERVED_DOM_EVENTS) {
+      removeEventListener(eventType, instance._onDropdownSizeUpdateNeeded)
+    }
+  }
+
+  function onDomMutated(instance) {
+    // Since we are mutating our subtree, there will be false positives, so we always need to check what has changed
+
+    const select = instance._select
+    if (select && ((select.multiple && instance._button) || (!select.multiple && !instance._button))) {
+      createUI(instance)
+    }
+  }
+
+  function onDropdownSizeUpdateNeeded(instance) {
+    if (!instance._dropdown || !instance._dropdown._broker || !instance._dropdownOptions._broker) {
+      return
+    }
+
+    const contentHeight = instance._dropdownOptions.scrollHeight
+    const dropdownStyle = getComputedStyle(instance._dropdownOptions)
+    const maxHeight = (
+      contentHeight + parseInt(dropdownStyle.borderTopWidth, 10) + parseInt(dropdownStyle.borderBottomWidth, 10)
+    )
+    const dropdownBounds = instance._dropdownContent.getBoundingClientRect()
+    const isTopAligned = instance._dropdown.verticalAlignment === instance._dropdown.VERTICAL_ALIGN.TOP
+    const viewportHeight = window.innerHeight
+
+    const suggestedHeight = isTopAligned ?
+      Math.min(maxHeight, dropdownBounds.bottom)
+      :
+      Math.min(maxHeight, viewportHeight - dropdownBounds.top)
+
+    const currentHeight = dropdownBounds.height || dropdownBounds.bottom - dropdownBounds.top
+
+    if (suggestedHeight !== currentHeight) {
+      instance._dropdownContent.style.height = `${suggestedHeight}px`
+    }
+  }
+
+  function onToggleDropdown(instance, event) {
+    event.stopPropagation()
+    // instance.setOpen(true / false)
+  }
+
+  function onDropdownPositionChange(instance, verticalAlignment) {
+    const isOpenedAtTop = verticalAlignment === instance._dropdown.VERTICAL_ALIGN.TOP
+    instance._dropdownPosition = verticalAlignment
+    if (instance._button && instance._button._broker) {
+      const {OPENING_POSITION} = instance._button
+      instance._button.setOpeningPosition(isOpenedAtTop ? OPENING_POSITION.UP : OPENING_POSITION.DOWN)
+    }
+    onDropdownSizeUpdateNeeded(instance)
+  }
+
+  function createUI(instance) {
+    clearUi(instance)
+
+    if (!instance._select) {
+      return
+    }
+
+    if (instance._select.multiple) {
+      createMultiSelectUi(instance)
+    } else {
+      createSingleSelectUi(instance)
+    }
+  }
+
+  function createSingleSelectUi(instance) {
+    initSingleSelectButton(instance)
+
+    instance._dropdownOptions = document.createElement('szn-options')
+    instance._dropdown = document.createElement('szn-tethered')
+    instance._dropdown.appendChild(instance._dropdownContent)
+    instance._dropdownContent.appendChild(instance._dropdownOptions)
+  }
+
+  function initSingleSelectButton(instance) {
+    const button = document.createElement('szn-select-button')
+    SznElements.awaitElementReady(button, () => {
+      if (instance._button !== button) {
+        return
+      }
+
+      instance._button.setSelectElement(instance._select)
+      if (instance._dropdown.parentNode) {
+        instance._button.setOpen(true)
+      }
+      if (instance._dropdownPosition) {
+        onDropdownPositionChange(instance, instance._dropdownPosition)
+      }
+    })
+
+    instance._button = button
+    instance._root.appendChild(button)
+  }
+
+  function initDropdown(instance, dropdown, options) {
+    dropdown.setTether(instance._root)
+    options.setOptions(instance._select)
+    dropdown.minBottomSpace = instance._minBottomSpace
+    dropdown.onVerticalAlignmentChange = instance._onDropdownPositionChange
+    instance._onDropdownPositionChange(dropdown.verticalAlignment)
+    onDropdownSizeUpdateNeeded(instance)
+  }
+
+  function createMultiSelectUi(instance) {
+    const select = instance._select
+    const options = document.createElement('szn-options')
+    instance._root.appendChild(options)
+    SznElements.awaitElementReady(options, () => options.setOptions(select))
+  }
+
+  function clearUi(instance) {
+    instance._root.innerHTML = ''
+    instance._dropdownContent.innerHTML = ''
+    if (instance._dropdown && instance._dropdown.parentNode) {
+      instance._dropdown.parentNode.removeChild(instance._dropdown)
+    }
+    instance._button = null
+    instance._dropdown = null
+    instance._dropdownPosition = null
+    instance._dropdownOptions = null
+  }
+
+  if (SznElements.init) {
+    SznElements.init()
+  }
+})(self)
